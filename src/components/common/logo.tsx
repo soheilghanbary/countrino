023375799725d8c18e562de7a3d9f302e77c606e
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-11-18 22:22:48 +0330

    add animation & logo icon

--- src/components/common/logo.tsx
+++ src/components/common/logo.tsx
@@ -1,9 +1,13 @@
-import Link from "next/link";
+import { Eclipse } from 'lucide-react';
+import Link from 'next/link';
 
 export function Logo() {
   return (
     <Link href={'/'}>
-      <span className="font-bold">Countrino</span>
+      <span className="flex items-center gap-2 font-bold">
+        <Eclipse className="size-5" strokeWidth={2.5} />
+        Countrino
+      </span>
     </Link>
   );
 }
